a10e6ad0bc8573f148a4a166f3054c9540af6f56
pirovanom <106339104+pirovanom@users.noreply.github.com>
2022-07-26 18:16:27 +0200

    Update Data_preparation.py

--- Data_preparation.py
+++ Data_preparation.py
@@ -1,23 +1,17 @@
-# DESCRIZIONE: Preparo il file csv di input per il modello
+# DESCRIZIONE: Preparo il file csv di input per il modello LSTM
 
 # importo le librerie
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
 
-# importo il file csv e creo il dataframe dati
+# Importo il file csv e creo il dataframe df_dati
 df_dati= pd.read_csv(r"/home/marta/python/fill_metadata_2017_10min.csv", usecols = [0,1,2,3,4,5,6,7,8])
-# escludo dal dataframe le linee con in_fill=0 e quelle che non corrispondono al fill scelto
+# Escludo dal dataframe le linee con in_fill=0 (e se decommento due righe quelle che non corrispondono al fill scelto)
 df_dati=df_dati.drop(df_dati[df_dati.in_fill==0].index)
 #fill = 5872
 #df_dati=df_dati.drop(df_dati[df_dati.fill_num!=fill].index)
-# converto il tempo dal formato timestamp a giorni
-#df_dati.time = [datetime.datetime.fromtimestamp(ts) for ts in df_dati.time]
-#print(date)
-#df_dati.time
-#df_dati.time=(df_dati.time/86400)-17309
-# escludo i dati dei fill non buoni
+# Escludo i dati dei fill non buoni
 escludi = list()
 escludi = [5697, 5698, 5699, 5704, 5710, 5718, 5719, 5722, 5733, 5736, 5738, 5739, 5740, 5746, 5749, 5822, 5824, 5825,
            5830, 5833, 5834, 5837, 5838, 5859, 5860, 5861, 5862, 5868, 5870, 5871, 5874, 5874, 5885, 5919, 5920, 5929, 
@@ -31,49 +25,54 @@ for k in escludi:
     df_dati=df_dati.drop(df_dati[df_dati.fill_num==k].index)
 
 # Creo un dataframe con i valori della trasparenza
-# importo il file npy e creo il dataframe trasparenza
+
+# Importo il file npy e creo il dataframe df_trasparenza
 trasparenza = np.load(r"/home/marta/python/2-TraspVsTime/iRing25new.npy")
 df_trasparenza = pd.DataFrame(trasparenza)
-# inverto righe e colonne del dataframe sulla trasparenza
+# Inverto righe e colonne del dataframe df_trasparenza
 df_trasparenza= df_trasparenza.transpose()
-# aggiungo una colonna al dataframe trasparenza, che poi conterra la media delle trasparenze
+# Aggiungo una colonna al dataframe df_trasparenza, che poi conterra la media delle trasparenze
 df_trasparenza=df_trasparenza.assign(trasparenza = 0)
-# faccio la somma per riga e divido per il numero di colonne per ottenere la media
+# Faccio la somma per riga e divido per il numero di colonne per ottenere la media
 df_trasparenza.trasparenza=df_trasparenza.sum(axis=1)/311
-# escludo dal dataframe le linee con trasparenza < 0
+# Escludo dal dataframe le linee con trasparenza < 0
 df_trasparenza=df_trasparenza.drop(df_trasparenza[df_trasparenza.trasparenza<0].index)
 
-# aggiungo la colonna trasparenza al dataframe dati
+# Aggiungo la colonna trasparenza al dataframe dati
 df_dati=df_dati.assign(trasparenza = 0)
 df_dati.trasparenza=df_trasparenza.trasparenza
 df_dati=df_dati.drop(df_dati[df_dati.trasparenza<0].index)
-#tolgo una colonna (in_fill) al dataframe dati
+# Tolgo una colonna (in_fill) al dataframe dati
 df_dati.drop(df_dati.columns[[0]], axis = 1, inplace = True)
-#sostituisco con 0 i valori NA
+# Sostituisco con 0 i valori NA
 df_dati['trasparenza'].fillna(0, inplace=True)
-#escludo dal dataframe le linee con trasparenza 0
+# Escludo dal dataframe le linee con trasparenza 0
 df_dati.drop(df_dati.loc[df_dati['trasparenza']==0.0].index, inplace=True)
 
-#normalizzo la trasparenza dividendo per il massimo valore di traspareza per ogni fill 
+# Normalizzo la trasparenza dividendo per il massimo valore di traspareza per ogni fill 
+
+# Creo una lista contente tutti i numeri di fill presenti nel dataframe df_dati
 fill_nums = df_dati.fill_num.unique()
+# Creo un dataframe vuoto
 dataframe = pd.DataFrame()
+# Per ogni fill scalo i valori di trasparenza e aggiungo il nuovo dataframe al dataframe vuoto appena creato
 for k in (fill_nums):
     new_df = df_dati[df_dati.fill_num == k]
     massimo_trasp = new_df.trasparenza.iloc[0]
     minimo_trasp = new_df['trasparenza'].min()
     new_df.trasparenza = new_df.trasparenza / massimo_trasp
-    #new_df.trasparenza = (new_df.trasparenza - minimo_trasp) / (massimo_trasp - minimo_trasp)
     dataframe = dataframe.append(new_df, ignore_index=True)
 
-# stampo il df dati
+# Stampo i dataframe 
 #print("df_dati")
 #print(df_dati)
 print("dataframe")
 print(dataframe)
 
+# Salvo il dataframe in un file csv
 dataframe.to_csv('/home/marta/python/5-Correlazioni/correlazioni.csv')
 
-#scalo a mano tutti i dati tranne la trasparenza
+# Scalo a mano tutti i dati tranne la trasparenza, per portarli nel range (0, 1) perchè così li vuole la rete neurale
 massimo = dataframe['time_in_fill'].max()
 minimo = dataframe['time_in_fill'].min()
 dataframe.time_in_fill = (dataframe.time_in_fill - minimo) / (massimo - minimo)
@@ -97,10 +96,6 @@ dataframe.lumi_int = (dataframe.lumi_int - minimo) / (massimo - minimo)
 massimo = dataframe['lumi_inst'].max()
 minimo = dataframe['lumi_inst'].min()
 dataframe.lumi_inst = (dataframe.lumi_inst - minimo) / (massimo - minimo)
-
-"""massimo = dataframe['time'].max()
-minimo = dataframe['time'].min()
-dataframe.time = (dataframe.time - minimo) / (massimo - minimo)"""
 
 plt.scatter(dataframe.time, dataframe.trasparenza)
 plt.title("Trasparenza")
@@ -134,9 +129,9 @@ plt.plot(dataframe.lumi_inst)
 plt.title("lumi_inst")
 plt.show()"""
 
-#faccio un file csv con solo i dati del tempo
+# Creo un file csv con solo i dati del tempo
+
 time = dataframe.copy()
-#time.set_index('indice', drop = False, inplace = True)
 time.drop(['lumi_inst'], axis = 1, inplace = True)
 time.drop(['lumi_int'], axis = 1, inplace = True)
 time.drop(['time_in_fill'], axis = 1, inplace = True)
@@ -145,9 +140,8 @@ time.drop(['fill_num'], axis = 1, inplace = True)
 time.drop(['lumi_in_fill'], axis = 1, inplace = True)
 time.drop(['lumi_last_fill'], axis = 1, inplace = True)
 time.drop(['trasparenza'], axis = 1, inplace = True)
-#time.drop(['trasp_preced'], axis = 1, inplace = True)
-print("time")
-print(time)
+#print("time")
+#print(time)
 time.set_index('time', drop = False, inplace = True)
 #time.time = [datetime.datetime.fromtimestamp(ts) for ts in time.time]
 #time.index = [datetime.datetime.fromtimestamp(ts) for ts in time.index]
@@ -155,32 +149,29 @@ print("time")
 print(time)
 time.to_csv('/home/marta/python/7-LSTM_prova2/time.csv')
 
-#faccio un file csv con solo i dati del numero di fill
+# Faccio un file csv con solo i dati del numero di fill
+
 fill_n = dataframe.copy()
-#time.set_index('indice', drop = False, inplace = True)
 fill_n.drop(['lumi_inst'], axis = 1, inplace = True)
 fill_n.drop(['lumi_int'], axis = 1, inplace = True)
 fill_n.drop(['time_in_fill'], axis = 1, inplace = True)
 fill_n.drop(['lumi_since_last_point'], axis = 1, inplace = True)
-#fill_n.drop(['time'], axis = 1, inplace = True)
 fill_n.drop(['lumi_in_fill'], axis = 1, inplace = True)
 fill_n.drop(['lumi_last_fill'], axis = 1, inplace = True)
 fill_n.drop(['trasparenza'], axis = 1, inplace = True)
-#time.drop(['trasp_preced'], axis = 1, inplace = True)
 fill_n.set_index('time', drop = True, inplace = True)
-#fill_n.index = [datetime.datetime.fromtimestamp(ts) for ts in fill_n.index]
 print("fill_n")
 print(fill_n)
 fill_n.to_csv('/home/marta/python/7-LSTM_prova2/fill_n.csv')
 
-#definisco la colonna time come indice
-#df_dati.set_index('time', drop = True, inplace = True)
+# Definisco la colonna time come indice
+
 dataframe.set_index('time', drop = True, inplace = True)
 print(dataframe)
 plt.plot(dataframe)
 plt.show()
 
-# salvo i dati in un file csv
+# Salvo i dati in un file csv, che userò come input per la rete neurale LSTM
 dataframe.to_csv('/home/marta/python/7-LSTM_prova2/input_data.csv')
 
 
